48a51c4b417dc30de35a327af0a720eb00b27487
--global <wojbor90@gmail.com>
2025-01-02 12:47:02 +0100

    changed to ts and vite added vuetifyjs framework

---
+++ relation-client/src/plugins/vuetify.ts
@@ -0,0 +1,8 @@
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles'
+
+export default createVuetify({
+    theme: {
+        defaultTheme: 'light',
+    },
+})
---
+++ relation-client/src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-container>
+    <h1>Welcome to the Home Page</h1>
+  </v-container>
+</template>
+<script lang="ts">
+
+export default {
+  name: 'Home',
+}
+</script>
