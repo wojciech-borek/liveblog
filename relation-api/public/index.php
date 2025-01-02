3ba9de8640ea71db9356bfe7d22417b07f3d444b
--global <wojbor90@gmail.com>
2025-01-02 23:32:00 +0100

    added cors allowed

--- relation-api/public/index.php
+++ relation-api/public/index.php
@@ -4,6 +4,17 @@ use App\Shared\Infrastructure\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Max-Age: 3600");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
