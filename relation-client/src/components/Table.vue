31a1697ef427a1d900c1fee481e6a5ba2708fc36
--global <wojbor90@gmail.com>
2025-01-07 23:45:02 +0100

    added response with pagination

--- relation-client/src/components/Table.vue
+++ relation-client/src/components/Table.vue
@@ -3,8 +3,7 @@
       :headers="columns"
       :items="items"
       :server-items-length="pagination?.totalCount || 0"
-      :options="localOptions"
-      @update:options="updateOptions"
+      @update:options="fetchData"
   >
     <template v-slot:top>
       <v-toolbar flat>
@@ -16,38 +15,16 @@
 </template>
 
 <script setup lang="ts">
-import { defineProps, defineEmits, ref, watch } from 'vue';
-import { Relation } from '../models';
-import { Pagination } from '@/services/Pagination.ts';
+import {defineProps, defineEmits, ref, watch} from 'vue';
+import {Relation} from '../models';
+import {Pagination} from '@/services/Pagination.ts';
 
 const props = defineProps<{
   columns: { title: string; key: string }[];
   items: Relation[];
   pagination: Pagination | null;
-  options: {
-    page: number;
-    itemsPerPage: number;
-    sortBy: string[];
-    sortDesc: boolean[];
-  };
+  fetchData: (options:{page: number, itemsPerPage: number, sortBy: string[], sortDesc: boolean[]}) => Promise<void>,
 }>();
 
-const emit = defineEmits(['update:options']);
 
-const localOptions = ref({ ...props.options });
-
-watch(
-    () => props.options,
-    (newOptions) => {
-      localOptions.value = { ...newOptions };
-    },
-    { deep: true }
-);
-
-const updateOptions = (newOptions: any) => {
-  if (JSON.stringify(newOptions) !== JSON.stringify(localOptions.value)) {
-    localOptions.value = { ...newOptions };
-    emit('update:options', newOptions);
-  }
-};
 </script>
--- relation-client/src/views/Relations.vue
+++ relation-client/src/views/Relations.vue
@@ -8,23 +8,22 @@
     ></v-progress-circular>
 
     <Table v-else
-           :options.sync="options"
            :pagination="pagination"
            :columns="columns"
            :items="relations"
-           @update:options="handleOptionsUpdate"
+           :fetchData="fetchData"
     />
   </v-container>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref } from 'vue';
+import {computed, defineComponent, ref} from 'vue';
 import Table from '../components/Table.vue';
-import { Relation } from '../models';
-import { getStatusTranslation } from '../enum/RelationStatusTranslation';
-import { useLoadingStore } from '../store/loadingStore.ts';
-import { RelationService } from '../services/RelationService';
-import { Pagination } from '../services/Pagination.ts';
+import {Relation} from '../models';
+import {getStatusTranslation} from '../enum/RelationStatusTranslation';
+import {useLoadingStore} from '../store/loadingStore.ts';
+import {RelationService} from '../services/RelationService';
+import {Pagination} from '../services/Pagination.ts';
 
 export default defineComponent({
   name: 'RelationView',
@@ -34,36 +33,28 @@ export default defineComponent({
 
   setup() {
     const columns = ref<{ title: string, key: string, value?: (item: any) => string }[]>([
-      { title: 'Nazwa', key: 'title' },
-      { title: 'Status', key: 'status', value: item => getStatusTranslation(item.status) },
+      {title: 'Nazwa', key: 'title'},
+      {title: 'Status', key: 'status', value: item => getStatusTranslation(item.status)},
     ]);
 
     const loadingStore = useLoadingStore();
     const relations = ref<Relation[]>([]);
     const pagination = ref<Pagination | null>(null);
     const isLoading = computed(() => loadingStore.isLoading);
-    const options = ref({
-      page: 1,
-      itemsPerPage: 10,
-      sortBy: [],
-      sortDesc: [],
-    });
 
-    const fetchData = async () => {
+    const fetchData = async (options: {
+      page: number;
+      itemsPerPage: number;
+      sortBy: string[];
+      sortDesc: boolean[]
+    }) => {
       try {
         // loadingStore.startLoading();
-        const result = await RelationService.getRelations(options.value.page, options.value.itemsPerPage);
+        const result = await RelationService.getRelations(options.page, options.itemsPerPage);
         relations.value = result.data;
         pagination.value = result.pagination;
       } finally {
         // loadingStore.stopLoading();
-      }
-    };
-
-    const handleOptionsUpdate = (newOptions: any) => {
-      if (JSON.stringify(newOptions) !== JSON.stringify(options.value)) {
-        options.value = newOptions;
-        fetchData();
       }
     };
 
@@ -72,8 +63,7 @@ export default defineComponent({
       relations,
       pagination,
       isLoading,
-      options,
-      handleOptionsUpdate,
+      fetchData,
     };
   },
 });
