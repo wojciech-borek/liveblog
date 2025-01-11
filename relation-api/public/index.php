10fb358b9bada4dbf6b95129e9ac6f3f3e81b058
wojbor <wojbor90@gmail.com>
2025-01-11 11:42:37 +0100

    permission for PUT and DELETE methods

--- relation-api/public/index.php
+++ relation-api/public/index.php
@@ -6,7 +6,7 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Max-Age: 3600");
 
