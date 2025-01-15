9933914eca487e50c31c4e2165a021e2ea8ec06c
wojbor <wojbor90@gmail.com>
2025-01-15 12:09:08 +0100

    removed unsed import class

--- relation-api/src/Relation/Application/Command/PostDelete/PostDeleteHandler.php
+++ relation-api/src/Relation/Application/Command/PostDelete/PostDeleteHandler.php
@@ -6,16 +6,8 @@ namespace App\Relation\Application\Command\PostDelete;
 use App\Relation\Application\Service\RelationService;
 use App\Relation\Domain\Exception\PostNotFoundException;
 use App\Relation\Domain\Exception\RelationNotFoundException;
-use App\Relation\Domain\Model\Post;
 use App\Relation\Domain\Repository\PostRepositoryInterface;
-use App\Relation\Domain\ValueObject\Post\IsPublished;
-use App\Relation\Domain\ValueObject\Post\PostContent;
 use App\Relation\Domain\ValueObject\Post\PostId;
-use App\Relation\Domain\ValueObject\Post\PostPosition;
-use App\Relation\Domain\ValueObject\Relation\RelationId;
-use App\Shared\Domain\ValueObject\CreatedAt;
-use App\Shared\Domain\ValueObject\ModifiedAt;
-use App\Shared\Infrastructure\Generator\MongoObjectIdGenerator;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
