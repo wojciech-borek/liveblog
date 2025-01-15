28c2b4775cf3b7de404d4df962f8940ba40756e7
wojbor <wojbor90@gmail.com>
2025-01-15 19:54:19 +0100

    removed serializer

--- relation-api/src/Relation/Presentation/Action/Post/CreatePostAction.php
+++ relation-api/src/Relation/Presentation/Action/Post/CreatePostAction.php
@@ -9,13 +9,12 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 use OpenApi\Attributes as OA;
 
 #[OA\Tag('Posts')]
 final readonly class CreatePostAction
 {
-    public function __construct(private MessageCommandBusInterface $messageBus, private SerializerInterface $serializer) {
+    public function __construct(private MessageCommandBusInterface $messageBus) {
     }
 
     #[Route('/api/posts', name: 'create_post', methods: ['POST'])]
